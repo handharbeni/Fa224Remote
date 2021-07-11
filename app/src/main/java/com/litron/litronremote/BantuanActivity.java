066a4e31d64be28535fa9a7ed187feccc0e4f038
Muhammad Handharbeni <mhandharbeni@gmail.com>
2021-07-11 23:19:24 +0700

    compability for ch34x

--- app/src/main/java/com/litron/litronremote/BantuanActivity.java
+++ app/src/main/java/com/litron/litronremote/BantuanActivity.java
@@ -4,15 +4,24 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 
 import com.crashlytics.android.Crashlytics;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
 public class BantuanActivity extends AppCompatActivity {
+    @BindView(R.id.tvVersion)
+    TextView tvVersion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bantuan);
+        ButterKnife.bind(this);
+
+        tvVersion.setText(String.format("v %s", BuildConfig.VERSION_NAME));
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
